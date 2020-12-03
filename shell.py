37c1d7398d2c2e6912ef25ac5b0894e8ab1ed934
Rich Foulkes <rbfoulkes@gmail.com>
2020-12-03 11:49:14 -0500

    Episode 2: Adding Parser
    its working good but gives no error messages.
    
    up to 9:40 in video.

--- shell.py
+++ shell.py
@@ -2,8 +2,8 @@ import basic
 
 while True:
     text = input('basic > ')
+    if text == 'end': break
     result, error = basic.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
-
