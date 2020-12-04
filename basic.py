e5482d03cecb2ad94d699cb56e4e9a838191c960
Rich Foulkes <rbfoulkes@gmail.com>
2020-12-03 21:34:17 -0500

    EP BONUS Finished
    Added Power operator.

--- basic.py
+++ basic.py
@@ -100,6 +100,7 @@ TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
 TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
+TT_POW      = 'POW'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
 TT_EOF      = 'EOF'
@@ -116,7 +117,6 @@ class Token:
 
         if pos_end:
             self.pos_end = pos_end
-
 
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
@@ -160,6 +160,9 @@ class Lexer:
                 self.advance()
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '^':
+                tokens.append(Token(TT_POW, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN, pos_start=self.pos))
@@ -262,7 +265,6 @@ class ParseResult:
 
 
 
-
 #############################
 # PARSER
 #############################
@@ -273,7 +275,7 @@ class Parser:
         self.tok_idx = -1
         self.advance()
 
-    def advance(self, ):
+    def advance(self):
         self.tok_idx += 1
         if self.tok_idx < len(self.tokens):
             self.current_tok = self.tokens[self.tok_idx]
@@ -290,17 +292,11 @@ class Parser:
 
     #############################
 
-    def factor(self):
+    def atom(self):
         res = ParseResult()
         tok = self.current_tok
 
-        if tok.type in (TT_PLUS, TT_MINUS):
-            res.register(self.advance())
-            factor = res.register(self.factor())
-            if res.error: return res
-            return res.success(UnaryOpNode(tok, factor))
-
-        elif tok.type in (TT_INT, TT_FLOAT):
+        if tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
         
@@ -317,7 +313,23 @@ class Parser:
                     self.current_tok.pos_end, \
                     "Expected ')'"))
 
-        return res.failure(InvalidSyntaxError(tok.pos_start, tok.pos_end, 'Expected int or float'))
+        return res.failure(InvalidSyntaxError( \
+            tok.pos_start, tok.pos_end, "Expected int, float, '+', '-', or '('"))
+
+    def power(self):
+        return self.bin_op(self.atom, (TT_POW, ), self.factor)
+
+    def factor(self):
+        res = ParseResult()
+        tok = self.current_tok
+
+        if tok.type in (TT_PLUS, TT_MINUS):
+            res.register(self.advance())
+            factor = res.register(self.factor())
+            if res.error: return res
+            return res.success(UnaryOpNode(tok, factor))
+
+        return self.power()
 
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
@@ -327,15 +339,18 @@ class Parser:
 
     #############################
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
+
         res = ParseResult()
-        left = res.register(func())
+        left = res.register(func_a())
         if res.error: return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
             res.register(self.advance())
-            right = res.register(func())
+            right = res.register(func_b())
             if res.error: return res
             left = BinOpNode(left, op_tok, right)
     
@@ -405,6 +420,11 @@ class Number:
                         self.context)
 
             return Number(self.value / other.value).set_context(self.context), None
+    
+    def powered_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value ** other.value).set_context(self.context), None
+
 
     def __repr__(self):
         return str(self.value)
@@ -457,6 +477,8 @@ class Interpreter:
             result, error = left.multiplied_by(right)
         elif node.op_tok.type == TT_DIV:
             result, error = left.divided_by(right)
+        elif node.op_tok.type == TT_POW:
+            result, error = left.powered_by(right)
 
         if error:
             return res.failure(error)
