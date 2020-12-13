5d98b6b109aac1b6dea5a131383079ee7342d7e0
Rich Foulkes <rbfoulkes@gmail.com>
2020-12-12 22:36:42 -0500

    EP14 Finished !!!!
    added RUN and comments to code.
    
    working good.

--- shell.py
+++ shell.py
@@ -3,17 +3,14 @@ import basic
 while True:
     text = input('basic > ')
     if text == 'end': break
+    if text.strip() == "": continue
+    if text.strip()[0] == '#': continue  # hit endless loop without this line.
     result, error = basic.run('<stdin>', text)
 
     if error: print(error.as_string())
-    elif result: print(result)
+    elif result: 
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
 
-
-# while True:
-#     text = '5 + 3'
-#     if text == 'end': break
-#     result, error = basic.run('<stdin>', text)
-
-#     if error: print(error.as_string())
-#     elif result: print(result)
-#     break
